3a553f1542186e5fcc547a7c92ae164985728825
davidn10 <david.newhouse@gmail.com>
2023-04-02 21:29:05 -0400

    Update summary.ebp.R
    bug fix

--- R/summary.ebp.R
+++ R/summary.ebp.R
@@ -52,7 +52,7 @@ summary.ebp <- function(object, ...) {
         round(object$transform_param$shift_par, 3),
       row.names = ""
     )
-    else if (object$transformation == "ordernorm") {
+  }  else if (object$transformation == "ordernorm") {
       transform_method <- data.frame(Transformation  = object$transformation,
                                      Shift_parameter = 0,
                                      row.names       = "" 
